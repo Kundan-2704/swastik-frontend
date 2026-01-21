15a712d223ca3572ac0fb97e9522fd14ab55ff15
Rajendra Dewangan <rajendradewangan2712@gmail.com>
2026-01-21 15:57:25 +0530

    Fix React JSX runtime config (final)

--- vite.config.ts
+++ vite.config.ts
@@ -10,19 +10,46 @@
 
 
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: process.env.VITE_API_BASE_URL, // 👈 BACKEND PORT
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+
+
+
+
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      jsxRuntime: "automatic"
+    }),
+    tailwindcss()
+  ],
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_API_BASE_URL, // 👈 BACKEND PORT
+        target: process.env.VITE_API_BASE_URL,
         changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+        secure: false
+      }
+    }
+  }
 });
