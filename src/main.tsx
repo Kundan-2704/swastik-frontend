739dc32decc0e2ce011fafb4902ba1b9e8df18fb
Rajendra Dewangan <rajendradewangan2712@gmail.com>
2026-01-21 17:35:13 +0530

    fix react runtime undefined

--- src/main.tsx
+++ src/main.tsx
@@ -44,7 +44,43 @@
 
 
 
-import React from "react";
+// import React from "react";
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+
+// import "./index.css";
+// import App from "./App";
+// import store from "./Redux Toolkit/Store";
+// import SmoothScrollProvider from "./providers/SmoothScrollProvider";
+
+// // 🔥 VERY IMPORTANT (browser ko interfere karne se roko)
+// if ("scrollRestoration" in window.history) {
+//   window.history.scrollRestoration = "manual";
+// }
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <BrowserRouter>
+//       <Provider store={store}>
+//         <SmoothScrollProvider>
+//           <App />
+//         </SmoothScrollProvider>
+//       </Provider>
+//     </BrowserRouter>
+//   </StrictMode>
+// );
+
+
+
+
+
+
+
+import * as React from "react";
+(window as any).React = React;
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -56,12 +92,18 @@ import App from "./App";
 import store from "./Redux Toolkit/Store";
 import SmoothScrollProvider from "./providers/SmoothScrollProvider";
 
-// 🔥 VERY IMPORTANT (browser ko interfere karne se roko)
+// 🔥 stop browser auto scroll restore
 if ("scrollRestoration" in window.history) {
   window.history.scrollRestoration = "manual";
 }
 
-createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Root element not found");
+}
+
+createRoot(root).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
